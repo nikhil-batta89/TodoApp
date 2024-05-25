4e71c0ab98e1e432093fd9244cdc38f6506aa720
nikhil-batta89 <125860348+nikhil-batta89@users.noreply.github.com>
2024-05-25 12:29:13 +0530

    Update Todo.js

--- models/Todo.js
+++ models/Todo.js
@@ -28,30 +28,3 @@ const todoSchema = new mongoose.Schema(
 module.exports = mongoose.model("Todo", todoSchema);
 
 
-// const mongoose = require('mongoose');
-
-// const todoSchema = new mongoose.Schema(
-//     {
-//         title:{
-//            type:String,
-//            required:true,
-//            maxLength:50,
-//         },
-//         description:{
-//            type:String,
-//            required:true,
-//            maxLength:50,
-//         },
-//         createdAt:{
-//            type:Date,
-//            required:true,
-//            default:Date.now(),
-//         },
-//         updatedAt:{
-//             type:Date,
-//             required:true,
-//             default:Date.now(),
-//         }
-//     }
-// );
-// module.exports = mongoose.model("Todo",todoSchema);
