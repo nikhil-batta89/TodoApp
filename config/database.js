b96650b135a648cbf6555fb3cb57df52e4a0b3c7
nikhil-batta89 <125860348+nikhil-batta89@users.noreply.github.com>
2024-05-25 12:27:35 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -18,24 +18,4 @@ const dbConnect = () => {
 
 module.exports = dbConnect;
 
-// const mongoose = require("mongoose");
 
-// require("dotenv").config();
-
-// const dbconnect = () =>{
-//     mongoose.connect(process.env.DATABASE_URL, {
-//         useNewUrlParser:true,
-//         useUnifiedTopology:true,
-//     })
-//     .then(()=>console.log("DB ka connection is Successful"))
-//     .catch( (err)=>
-//     {
-//         console.log("Issue in DB connection");
-//         console.error(err.message);
-//         process.exit(1);
-// });
-// }
-// module.exports = dbconnect;
-//connection ko call karuga function call kara do ga
-
-//function ke under wrap kar deya or export kar vadiya
