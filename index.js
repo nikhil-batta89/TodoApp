b10c97f2241e1f95b70c67a520ddf6bff6353a01
nikhil-batta89 <125860348+nikhil-batta89@users.noreply.github.com>
2024-05-25 12:30:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -30,34 +30,3 @@ app.get("/", (req,res) => {
 })
 
 
-
-
-
-// const express = require("express");
-// const app = express();
-
-// //load config from env.file
-// require("dotenv").config();
-// const PORT = process.env.PORT || 4000;
-
-// //middleware to parse json request body
-// app.use(express.json());
-// //import routes for ToDO Api
-// const todoRoutes = require
-// //all routes to mount the todo api routes
-// app.use("/api/v1",todoRoutes);
-// // local host port no  v1 , route define 
-// //ha controler ka vo ajaga ga menas full URL ayajaga
-// //start server
-// app.listen(PORT,()=>{
-//     console.log(`Server started successfully at ${PORT}`);
-// })
-
-// //connect to the database
-// const dbConnect = require("./config/database");
-// dbConnect();
-
-// //default Route
-// app.get("/",(req,res)=>{
-//     res.send(`<h1>Thisis HOMEPAGE BAby</h1>`)
-// })
